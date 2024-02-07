6c420bdfc23eb3ee5b431e32fcfa63671a32f922
Nguyễn Minh Đức <118814799+mduc21082003@users.noreply.github.com>
2024-02-07 18:15:23 +0700

    them form dang nhap

---
+++ Tesing/DangNhap.Designer.cs
@@ -0,0 +1,295 @@
+﻿
+namespace Tesing
+{
+    partial class DangNhap
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new System.Windows.Forms.Panel();
+            lbDangNhap = new System.Windows.Forms.Label();
+            panel2 = new System.Windows.Forms.Panel();
+            panel5 = new System.Windows.Forms.Panel();
+            llbQuenMatKhau = new System.Windows.Forms.LinkLabel();
+            cbNhoTaiKhoan = new System.Windows.Forms.CheckBox();
+            panel4 = new System.Windows.Forms.Panel();
+            txbMatKhau = new System.Windows.Forms.TextBox();
+            lbMatKhau = new System.Windows.Forms.Label();
+            panel3 = new System.Windows.Forms.Panel();
+            txbDangNhap = new System.Windows.Forms.TextBox();
+            lbTenDangNhap = new System.Windows.Forms.Label();
+            label4 = new System.Windows.Forms.Label();
+            btDangNhap = new System.Windows.Forms.Button();
+            label5 = new System.Windows.Forms.Label();
+            linkLabel2 = new System.Windows.Forms.LinkLabel();
+            panel6 = new System.Windows.Forms.Panel();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            panel5.SuspendLayout();
+            panel4.SuspendLayout();
+            panel3.SuspendLayout();
+            panel6.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = System.Drawing.SystemColors.Highlight;
+            panel1.Controls.Add(lbDangNhap);
+            panel1.Location = new System.Drawing.Point(-5, 1);
+            panel1.Margin = new System.Windows.Forms.Padding(4);
+            panel1.Name = "panel1";
+            panel1.Size = new System.Drawing.Size(911, 121);
+            panel1.TabIndex = 0;
+            // 
+            // lbDangNhap
+            // 
+            lbDangNhap.AutoSize = true;
+            lbDangNhap.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            lbDangNhap.ForeColor = System.Drawing.Color.White;
+            lbDangNhap.Location = new System.Drawing.Point(375, 37);
+            lbDangNhap.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            lbDangNhap.Name = "lbDangNhap";
+            lbDangNhap.Size = new System.Drawing.Size(161, 38);
+            lbDangNhap.TabIndex = 0;
+            lbDangNhap.Text = "Đăng nhập";
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(panel5);
+            panel2.Controls.Add(panel4);
+            panel2.Controls.Add(panel3);
+            panel2.Location = new System.Drawing.Point(2, 123);
+            panel2.Margin = new System.Windows.Forms.Padding(4);
+            panel2.Name = "panel2";
+            panel2.Size = new System.Drawing.Size(899, 280);
+            panel2.TabIndex = 1;
+            // 
+            // panel5
+            // 
+            panel5.BackColor = System.Drawing.SystemColors.Control;
+            panel5.Controls.Add(llbQuenMatKhau);
+            panel5.Controls.Add(cbNhoTaiKhoan);
+            panel5.Location = new System.Drawing.Point(123, 219);
+            panel5.Margin = new System.Windows.Forms.Padding(4);
+            panel5.Name = "panel5";
+            panel5.Size = new System.Drawing.Size(652, 40);
+            panel5.TabIndex = 2;
+            // 
+            // llbQuenMatKhau
+            // 
+            llbQuenMatKhau.AutoSize = true;
+            llbQuenMatKhau.Location = new System.Drawing.Point(530, 0);
+            llbQuenMatKhau.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            llbQuenMatKhau.Name = "llbQuenMatKhau";
+            llbQuenMatKhau.Size = new System.Drawing.Size(129, 23);
+            llbQuenMatKhau.TabIndex = 1;
+            llbQuenMatKhau.TabStop = true;
+            llbQuenMatKhau.Text = "Quên mật khẩu";
+            // 
+            // cbNhoTaiKhoan
+            // 
+            cbNhoTaiKhoan.AutoSize = true;
+            cbNhoTaiKhoan.Location = new System.Drawing.Point(0, 0);
+            cbNhoTaiKhoan.Margin = new System.Windows.Forms.Padding(4);
+            cbNhoTaiKhoan.Name = "cbNhoTaiKhoan";
+            cbNhoTaiKhoan.Size = new System.Drawing.Size(141, 27);
+            cbNhoTaiKhoan.TabIndex = 0;
+            cbNhoTaiKhoan.Text = "Nhớ tài khoản";
+            cbNhoTaiKhoan.UseVisualStyleBackColor = true;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = System.Drawing.SystemColors.Control;
+            panel4.Controls.Add(txbMatKhau);
+            panel4.Controls.Add(lbMatKhau);
+            panel4.Location = new System.Drawing.Point(123, 121);
+            panel4.Margin = new System.Windows.Forms.Padding(4);
+            panel4.Name = "panel4";
+            panel4.Size = new System.Drawing.Size(652, 74);
+            panel4.TabIndex = 1;
+            // 
+            // txbMatKhau
+            // 
+            txbMatKhau.Location = new System.Drawing.Point(0, 42);
+            txbMatKhau.Margin = new System.Windows.Forms.Padding(4);
+            txbMatKhau.Name = "txbMatKhau";
+            txbMatKhau.Size = new System.Drawing.Size(652, 30);
+            txbMatKhau.TabIndex = 1;
+            txbMatKhau.UseSystemPasswordChar = true;
+            // 
+            // lbMatKhau
+            // 
+            lbMatKhau.AutoSize = true;
+            lbMatKhau.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            lbMatKhau.Location = new System.Drawing.Point(0, 0);
+            lbMatKhau.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            lbMatKhau.Name = "lbMatKhau";
+            lbMatKhau.Size = new System.Drawing.Size(93, 25);
+            lbMatKhau.TabIndex = 0;
+            lbMatKhau.Text = "Mật khẩu";
+            // 
+            // panel3
+            // 
+            panel3.BackColor = System.Drawing.SystemColors.Control;
+            panel3.Controls.Add(txbDangNhap);
+            panel3.Controls.Add(lbTenDangNhap);
+            panel3.Location = new System.Drawing.Point(123, 22);
+            panel3.Margin = new System.Windows.Forms.Padding(4);
+            panel3.Name = "panel3";
+            panel3.Size = new System.Drawing.Size(652, 74);
+            panel3.TabIndex = 0;
+            // 
+            // txbDangNhap
+            // 
+            txbDangNhap.Location = new System.Drawing.Point(0, 42);
+            txbDangNhap.Margin = new System.Windows.Forms.Padding(4);
+            txbDangNhap.Name = "txbDangNhap";
+            txbDangNhap.Size = new System.Drawing.Size(652, 30);
+            txbDangNhap.TabIndex = 1;
+            // 
+            // lbTenDangNhap
+            // 
+            lbTenDangNhap.AutoSize = true;
+            lbTenDangNhap.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            lbTenDangNhap.ForeColor = System.Drawing.SystemColors.Desktop;
+            lbTenDangNhap.Location = new System.Drawing.Point(0, 0);
+            lbTenDangNhap.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            lbTenDangNhap.Name = "lbTenDangNhap";
+            lbTenDangNhap.Size = new System.Drawing.Size(143, 25);
+            lbTenDangNhap.TabIndex = 0;
+            lbTenDangNhap.Text = "Tên đăng nhập:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.ForeColor = System.Drawing.SystemColors.Control;
+            label4.Location = new System.Drawing.Point(319, 15);
+            label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            label4.Name = "label4";
+            label4.Size = new System.Drawing.Size(273, 23);
+            label4.TabIndex = 0;
+            label4.Text = "Nhập đầy đủ thông tin đăng nhập";
+            // 
+            // btDangNhap
+            // 
+            btDangNhap.BackColor = System.Drawing.SystemColors.Control;
+            btDangNhap.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            btDangNhap.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            btDangNhap.ForeColor = System.Drawing.SystemColors.Desktop;
+            btDangNhap.Location = new System.Drawing.Point(348, 59);
+            btDangNhap.Margin = new System.Windows.Forms.Padding(4);
+            btDangNhap.Name = "btDangNhap";
+            btDangNhap.Size = new System.Drawing.Size(214, 38);
+            btDangNhap.TabIndex = 1;
+            btDangNhap.Text = "Đăng nhập";
+            btDangNhap.UseVisualStyleBackColor = false;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new System.Drawing.Point(302, 114);
+            label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            label5.Name = "label5";
+            label5.Size = new System.Drawing.Size(251, 23);
+            label5.TabIndex = 2;
+            label5.Text = "Bạn chưa có tài khoản? Đăng kí";
+            // 
+            // linkLabel2
+            // 
+            linkLabel2.AutoSize = true;
+            linkLabel2.Location = new System.Drawing.Point(548, 114);
+            linkLabel2.Name = "linkLabel2";
+            linkLabel2.Size = new System.Drawing.Size(61, 23);
+            linkLabel2.TabIndex = 3;
+            linkLabel2.TabStop = true;
+            linkLabel2.Text = "tại đây";
+            // 
+            // panel6
+            // 
+            panel6.BackColor = System.Drawing.SystemColors.Highlight;
+            panel6.Controls.Add(linkLabel2);
+            panel6.Controls.Add(label5);
+            panel6.Controls.Add(btDangNhap);
+            panel6.Controls.Add(label4);
+            panel6.ForeColor = System.Drawing.SystemColors.Control;
+            panel6.Location = new System.Drawing.Point(-5, 403);
+            panel6.Margin = new System.Windows.Forms.Padding(4);
+            panel6.Name = "panel6";
+            panel6.Size = new System.Drawing.Size(911, 156);
+            panel6.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(9F, 23F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(900, 558);
+            Controls.Add(panel6);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            Margin = new System.Windows.Forms.Padding(4);
+            Name = "Form1";
+            StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            Text = "Đăng nhập";
+            FormClosing += Form1_FormClosing;
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel5.ResumeLayout(false);
+            panel5.PerformLayout();
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            panel6.ResumeLayout(false);
+            panel6.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lbDangNhap;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.LinkLabel llbQuenMatKhau;
+        private System.Windows.Forms.CheckBox cbNhoTaiKhoan;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btDangNhap;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.TextBox txbMatKhau;
+        private System.Windows.Forms.Label lbMatKhau;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox txbDangNhap;
+        private System.Windows.Forms.Label lbTenDangNhap;
+    }
+}
+
